7cdffc98fff6db03ee45c6896b5984ea62c33bfd
Hamool <hamool111@gmail.com>
2025-09-17 22:04:16 +0700

    change to build fzf and nvim from source

--- dev-env-setup.sh
+++ dev-env-setup.sh
@@ -17,25 +17,80 @@ elif command -v pacman >/dev/null 2>&1; then
   sudo pacman -Sy --noconfirm
 fi
 
-PACKAGES=(nvim tmux git htop wget curl fzf)
 TO_INSTALL=()
 
-echo "Installing packages"
+mapfile -t PACKAGES < packages.txt
+
+# build fzf from source and add config to bashrc
+
+remove_package() {
+  local remove=$1
+  local new_pkgs=()
+
+  for pkg in "${PACKAGES[@]}"; do
+    if [[ "$pkg" != "$remove" ]]; then
+      new_pkgs+=("$pkg")
+    fi
+  done
+  PACKAGES=("${new_pkgs[@]}")
+}
+
+install_packages() {
+  if command -v apt >/dev/null 2>&1; then 
+    sudo apt install -y $1
+  elif command -v dnf >/dev/null 2>&1; then 
+    sudo dnf install -y $1
+  elif command -v pacman >/dev/null 2>&1; then 
+    sudo pacman -S --noconfirm $1
+  fi
+}
+
+if [[ "${PACKAGES[@]}" =~ "fzf" ]]; then 
+  echo "Building fzf from source"
+  remove_package "fzf"
+  if [[ ! -d $HOME/.fzf ]]; then
+    git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
+    $HOME/.fzf/install --all
+  else
+    echo "fzf already cloned to $HOME/.fzf"
+  fi
+fi
+
+if [[ "${PACKAGES[@]}" =~ "nvim" ]]; then
+  echo "Building nvim from source"
+  remove_package "nvim"
+
+  # Install build dependencies (Debian/Ubuntu/Fedora/Arch)
+  if command -v apt >/dev/null 2>&1; then
+    sudo apt install -y ninja-build gettext cmake unzip curl build-essential
+  elif command -v dnf >/dev/null 2>&1; then
+    sudo dnf install -y ninja-build libtool gcc make cmake curl unzip gettext
+  elif command -v pacman >/dev/null 2>&1; then
+    sudo pacman -S --noconfirm base-devel cmake unzip ninja curl gettext
+  else
+    echo "⚠️  Unknown package manager. Please install Neovim build deps manually."
+  fi
+
+  nvim_home=$HOME/nvim_src
+  if [[ ! -d "$nvim_home" ]]; then
+    git clone https://github.com/neovim/neovim.git "$nvim_home"
+  fi
+    cd $nvim_home
+    git pull
+    make CMAKE_BUILD_TYPE=Release
+    sudo make install
+    cd -
+fi
+
 for pkg in "${PACKAGES[@]}"; do
   if ! command -v "$pkg" >/dev/null 2>&1; then
     TO_INSTALL+=("$pkg")
   fi
 done
-if [ ${#TO_INSTALL[@]} -gt 0 ]; then 
-  if command -v apt >/dev/null 2>&1; then 
-    sudo apt install -y ${TO_INSTALL[*]}
-  elif command -v dnf >/dev/null 2>&1; then 
-    sudo dnf install -y ${TO_INSTALL[*]}
-  elif command -v pacman >/dev/null 2>&1; then 
-    sudo pacman -S --noconfirm ${TO_INSTALL[*]}
-  fi
-else
+if [ ! ${#TO_INSTALL[@]} -gt 0 ]; then 
   echo "Nothing to install"
+else
+  install_packages ${PACKAGES[@]}
 fi
 # update package database and install packages
 
